17a9afc80c32f7ea114243336013742e3031c59d
Rico <r.jambor@yahoo.de>
2018-01-30 01:11:20 +0100

    TASK: Add setup files for packaging

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+packages = find_packages()
+
+setup(name='pyBrematic',
+      version='0.2',
+      keywords='python telegram bot api wrapper',
+      description='Python code for controlling Brematic remote power outlets and potentially other stuff',
+      url='https://github.com/d-Rickyy-b/pyBrematic',
+      author='d-Rickyy-b',
+      author_email='pyBrematic@gmail.com',
+      license='MIT',
+      packages=packages,
+      zip_safe=False)
