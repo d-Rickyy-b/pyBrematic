b97a09796cfa757169fca23e0206d7dd5d73b043
Rico <r.jambor@yahoo.de>
2018-01-30 01:10:52 +0100

    TASK: Add __init__ files for packaging

---
+++ pyBrematic/__init__.py
@@ -0,0 +1,5 @@
+from .devices.device import Device
+from .gateways.brennenstuhl_gateway import BrennenstuhlGateway
+from .gateways.gateway import Gateway
+
+__all__ = ('Device', 'Gateway', 'BrennenstuhlGateway')
