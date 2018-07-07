62a7e67920cc17d01206e74afd0a7f6b64d53c33
Rico <r.jambor@yahoo.de>
2018-07-07 04:45:10 +0200

    TASK: Update version in setup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 packages = find_packages()
 
 setup(name='pyBrematic',
-      version='0.2',
+      version='1.0',
       keywords='python telegram bot api wrapper',
       description='Python code for controlling Brematic remote power outlets and potentially other stuff',
       url='https://github.com/d-Rickyy-b/pyBrematic',
