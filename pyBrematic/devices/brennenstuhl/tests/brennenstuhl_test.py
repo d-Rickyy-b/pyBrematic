3fee3ed6f44fe665fb3049ebcfa97e35d97fc2e4
Rico <r.jambor@yahoo.de>
2018-07-07 03:51:22 +0200

    TASK: Optimize brennenstuhl tests

--- pyBrematic/devices/brennenstuhl/tests/brennenstuhl_test.py
+++ pyBrematic/devices/brennenstuhl/tests/brennenstuhl_test.py
@@ -2,8 +2,8 @@
 
 import unittest
 
-from pyBrematic.devices.brennenstuhl import RCR1000N, RCS1000N
 from pyBrematic.devices import Device
+from pyBrematic.devices.brennenstuhl import RCR1000N, RCS1000N
 from pyBrematic.gateways import BrennenstuhlGateway, IntertechnoGateway
 
 
@@ -17,6 +17,7 @@ class Brennenstuhl(unittest.TestCase):
         pass
 
     def test_RCR1000N(self):
+        """Test to check the functionality of the RCR1000N class"""
         dev = RCR1000N("10000", "00100")
 
         on_signal_ITGW = "0,0,10,11200,350,26,0,1,3,1,3,1,3,3,1,1,3,3,1,1,3,3,1,1,3,3,1,1,3,3,1,1,3,3,1,1,3,1,3,1,3,3,1,1,3,3,1,1,3,1,3,1,3,3,1,1,32,0"
@@ -25,19 +26,23 @@ class Brennenstuhl(unittest.TestCase):
         self.assertEqual(on_signal_ITGW, dev.get_signal(self.itgw, Device.ACTION_ON))
         self.assertEqual(off_signal_ITGW, dev.get_signal(self.itgw, Device.ACTION_OFF))
 
-        on_signal_BSGW =  "TXP:0,0,10,5600,350,25,1,3,1,3,1,3,3,1,1,3,3,1,1,3,3,1,1,3,3,1,1,3,3,1,1,3,3,1,1,3,1,3,1,3,3,1,1,3,3,1,1,3,1,3,1,3,3,1,1,16;"
+        on_signal_BSGW = "TXP:0,0,10,5600,350,25,1,3,1,3,1,3,3,1,1,3,3,1,1,3,3,1,1,3,3,1,1,3,3,1,1,3,3,1,1,3,1,3,1,3,3,1,1,3,3,1,1,3,1,3,1,3,3,1,1,16;"
         off_signal_BSGW = "TXP:0,0,10,5600,350,25,1,3,1,3,1,3,3,1,1,3,3,1,1,3,3,1,1,3,3,1,1,3,3,1,1,3,3,1,1,3,1,3,1,3,3,1,1,3,3,1,1,3,3,1,1,3,1,3,1,16;"
 
         self.assertEqual(on_signal_BSGW, dev.get_signal(self.bsgw, Device.ACTION_ON))
         self.assertEqual(off_signal_BSGW, dev.get_signal(self.bsgw, Device.ACTION_OFF))
 
     def test_RCS1000N(self):
+        """Test to check the functionality of the RCS1000N class"""
         dev = RCS1000N("10000", "00100")
 
         on_signal_ITGW = "0,0,10,11200,350,26,0,1,3,1,3,1,3,3,1,1,3,3,1,1,3,3,1,1,3,3,1,1,3,3,1,1,3,3,1,1,3,1,3,1,3,3,1,1,3,3,1,1,3,1,3,1,3,3,1,1,32,0"
         off_signal_ITGW = "0,0,10,11200,350,26,0,1,3,1,3,1,3,3,1,1,3,3,1,1,3,3,1,1,3,3,1,1,3,3,1,1,3,3,1,1,3,1,3,1,3,3,1,1,3,3,1,1,3,3,1,1,3,1,3,1,32,0"
 
-        on_signal_BSGW =  "TXP:0,0,10,5600,350,25,1,3,1,3,1,3,3,1,1,3,3,1,1,3,3,1,1,3,3,1,1,3,3,1,1,3,3,1,1,3,1,3,1,3,3,1,1,3,3,1,1,3,1,3,1,3,3,1,1,16;"
+        self.assertEqual(on_signal_ITGW, dev.get_signal(self.itgw, Device.ACTION_ON))
+        self.assertEqual(off_signal_ITGW, dev.get_signal(self.itgw, Device.ACTION_OFF))
+
+        on_signal_BSGW = "TXP:0,0,10,5600,350,25,1,3,1,3,1,3,3,1,1,3,3,1,1,3,3,1,1,3,3,1,1,3,3,1,1,3,3,1,1,3,1,3,1,3,3,1,1,3,3,1,1,3,1,3,1,3,3,1,1,16;"
         off_signal_BSGW = "TXP:0,0,10,5600,350,25,1,3,1,3,1,3,3,1,1,3,3,1,1,3,3,1,1,3,3,1,1,3,3,1,1,3,3,1,1,3,1,3,1,3,3,1,1,3,3,1,1,3,3,1,1,3,1,3,1,16;"
 
         self.assertEqual(on_signal_BSGW, dev.get_signal(self.bsgw, Device.ACTION_ON))
