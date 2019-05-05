bd5c56b23c8fad9710d7ff81fc7ad0978865d47b
Rico <d-Rickyy-b@users.noreply.github.com>
2019-05-06 00:54:28 +0200

    TASK: Add Action to init file

--- pyBrematic/devices/__init__.py
+++ pyBrematic/devices/__init__.py
@@ -4,4 +4,4 @@ from .device import Device
 from .autopairdevice import AutoPairDevice
 from .action import Action
 
-__all__ = ['Device', 'AutoPairDevice']
+__all__ = ['Device', 'AutoPairDevice', 'Action']
