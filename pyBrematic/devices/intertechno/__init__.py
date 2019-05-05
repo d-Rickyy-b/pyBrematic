4610caf10750850e2ad318cac205d9424b299060
Rico <d-Rickyy-b@users.noreply.github.com>
2019-05-06 00:47:29 +0200

    TASK: Add ITL500 to init file

--- pyBrematic/devices/intertechno/__init__.py
+++ pyBrematic/devices/intertechno/__init__.py
@@ -3,5 +3,6 @@
 from .CMR1000 import CMR1000
 from .PAR1500 import PAR1500
 from .ITR3500 import ITR3500
+from .ITL500 import ITL500
 
-__all__ = ['CMR1000', 'PAR1500', 'ITR3500']
+__all__ = ['CMR1000', 'PAR1500', 'ITR3500', 'ITL500']
