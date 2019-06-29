a389c71a1c8290cddf8b01106b2fdca4498d9548
Rico <d-Rickyy-b@users.noreply.github.com>
2019-06-29 22:04:00 +0200

    TASK: Split up unit tests more granularily

--- pyBrematic/devices/intertechno/tests/ITL500_test.py
+++ pyBrematic/devices/intertechno/tests/ITL500_test.py
@@ -14,21 +14,23 @@ class TestITL500(unittest.TestCase):
         seed = 8712387
         self.dev = ITL500(123, seed)
 
-    def test_up(self):
+    def test_up_intertechno(self):
         up_signal_ITGW = "0,0,5,10976,98,67,0,3,29," + "15,3,15,15,3,15,15,3,15,15,3,3,15,3,3,15,15,15,3,15,15,15,15,3,15,3,3,3,15,3,3,3,15,3,15,3,3,3,3,3,3,3,15,3,3,3,15," + "3,112,0"
         self.assertEqual(up_signal_ITGW, self.dev.get_signal(self.itgw, AutoPairDevice.ACTION_UP))
 
+    def test_up_brennenstuhl(self):
         up_signal_BSGW = "TXP:0,0,5,10976,98,66,3,29," + "15,3,15,15,3,15,15,3,15,15,3,3,15,3,3,15,15,15,3,15,15,15,15,3,15,3,3,3,15,3,3,3,15,3,15,3,3,3,3,3,3,3,15,3,3,3,15," + "3,126"
         self.assertEqual(up_signal_BSGW, self.dev.get_signal(self.bsgw, AutoPairDevice.ACTION_UP))
 
-    def test_down(self):
+    def test_down_intertechno(self):
         down_signal_ITGW = "0,0,5,10976,98,67,0,3,29," + "15,3,15,15,3,15,15,3,15,15,3,3,15,3,3,15,15,15,3,15,15,15,15,3,15,3,3,3,15,3,3,3,15,3,3,3,15,3,3,3,3,3,15,3,3,3,15," + "3,112,0"
         self.assertEqual(down_signal_ITGW, self.dev.get_signal(self.itgw, AutoPairDevice.ACTION_DOWN))
 
+    def test_down_brennenstuhl(self):
         down_signal_BSGW = "TXP:0,0,5,10976,98,66,3,29," + "15,3,15,15,3,15,15,3,15,15,3,3,15,3,3,15,15,15,3,15,15,15,15,3,15,3,3,3,15,3,3,3,15,3,3,3,15,3,3,3,3,3,15,3,3,3,15," + "3,126"
         self.assertEqual(down_signal_BSGW, self.dev.get_signal(self.bsgw, AutoPairDevice.ACTION_DOWN))
 
-    def test_other_seed(self):
+    def test_other_seed_intertechno(self):
         seed2 = 89090823173454879234
         dev_new = ITL500(1234, seed2)
 
@@ -36,6 +38,10 @@ class TestITL500(unittest.TestCase):
         down_signal_ITGW = "0,0,5,10976,98,67,0,3,29," + "15,3,15,15,3,3,3,3,3,15,3,3,15,15,3,3,15,15,3,3,15,15,15,3,15,3,3,3,15,3,3,3,15,3,3,3,15,3,3,3,3,3,15,3,3,3,15," + "3,112,0"
         self.assertEqual(up_signal_ITGW, dev_new.get_signal(self.itgw, AutoPairDevice.ACTION_UP))
         self.assertEqual(down_signal_ITGW, dev_new.get_signal(self.itgw, AutoPairDevice.ACTION_DOWN))
+
+    def test_other_seed_brennenstuhl(self):
+        seed2 = 89090823173454879234
+        dev_new = ITL500(1234, seed2)
 
         up_signal_BSGW = "TXP:0,0,5,10976,98,66,3,29," + "15,3,15,15,3,3,3,3,3,15,3,3,15,15,3,3,15,15,3,3,15,15,15,3,15,3,3,3,15,3,3,3,15,3,15,3,3,3,3,3,3,3,15,3,3,3,15," + "3,126"
         down_signal_BSGW = "TXP:0,0,5,10976,98,66,3,29," + "15,3,15,15,3,3,3,3,3,15,3,3,15,15,3,3,15,15,3,3,15,15,15,3,15,3,3,3,15,3,3,3,15,3,3,3,15,3,3,3,3,3,15,3,3,3,15," + "3,126"
