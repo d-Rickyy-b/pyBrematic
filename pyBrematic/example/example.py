7cb93f5b7d73bb20c4a675a052690c7ad16f9037
Rico <d-Rickyy-b@users.noreply.github.com>
2020-12-31 18:53:34 +0100

    feat: add code calculation to example

--- pyBrematic/example/example.py
+++ pyBrematic/example/example.py
@@ -3,6 +3,8 @@
 
 from pyBrematic.devices import Action
 from pyBrematic.devices.brennenstuhl import RCS1000N
+from pyBrematic.devices.intertechno import ITR3500
+from pyBrematic.devices.intertechno import calc_unitcode, calc_systemcode
 from pyBrematic.gateways import BrennenstuhlGateway
 
 # Set your system and unit codes
@@ -11,6 +13,11 @@ unit_code = "10000"  # Switch A is in the 'up' position, B-E are 'down'
 
 # Create a new device with the specified codes
 desk_lamp = RCS1000N(system_code, unit_code)
+
+# You can also use the following methods to get the code via master/slave (letter/number) notation
+led_system_code = calc_systemcode("A")
+led_unit_code = calc_unitcode(3)
+led_strip = ITR3500(led_system_code, led_unit_code)
 
 # Create a new gateway located at the specified IP
 gw = BrennenstuhlGateway("192.168.178.9")
