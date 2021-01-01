7127ca75ff1cac1d116f5c8e4ac7d177cb643b16
Rico <d-Rickyy-b@users.noreply.github.com>
2021-01-01 23:25:08 +0100

    style: use snake case for function name

--- pyBrematic/utils/singleton.py
+++ pyBrematic/utils/singleton.py
@@ -6,14 +6,14 @@ __instances = {}
 
 def singleton(cls):
     @wraps(cls)
-    def getInstance(*args, **kwargs):
+    def get_instance(*args, **kwargs):
         instance = __instances.get(cls, None)
         if not instance:
             instance = cls(*args, **kwargs)
             __instances[cls] = instance
         return instance
 
-    return getInstance
+    return get_instance
 
 
 def _delete_all_instances():
