a22026deb13ad6d7c4057e99198645643aa4f282
Rico <d-Rickyy-b@users.noreply.github.com>
2021-01-02 15:32:53 +0100

    refactor: use combined code calc method in example file

--- pyBrematic/example/example.py
+++ pyBrematic/example/example.py
@@ -3,20 +3,19 @@
 
 from pyBrematic import Action
 from pyBrematic.devices.brennenstuhl import RCS1000N
-from pyBrematic.devices.intertechno import ITR3500
-from pyBrematic.devices.intertechno import calc_unitcode, calc_systemcode
+from pyBrematic.devices.intertechno import ITR3500, calc_system_and_unit_code
 from pyBrematic.gateways import BrennenstuhlGateway
 
 # Set your system and unit codes
-system_code = "11110"  # Switches 1-4 are in the 'up' position, 5 is 'down'
-unit_code = "10000"  # Switch A is in the 'up' position, B-E are 'down'
+system_code = "11110"  # DIPs 1-4 are in the 'up' position, 5 is 'down'
+unit_code = "10000"  # DIP A is in the 'up' position, B-E are 'down'
 
 # Create a new device with the specified codes
 desk_lamp = RCS1000N(system_code, unit_code)
 
-# You can also use the following methods to get the code via master/slave (letter/number) notation
-led_system_code = calc_systemcode("A")
-led_unit_code = calc_unitcode(3)
+# For Intertechno devices you can also use the following methods to get the code
+# via master/slave (letter/number) notation. Allowed values are "A-P" and "1-16".
+led_system_code, led_unit_code = calc_system_and_unit_code("A3")
 led_strip = ITR3500(led_system_code, led_unit_code)
 
 # Create a new gateway located at the specified IP
