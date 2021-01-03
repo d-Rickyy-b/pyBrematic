5dd84022fc3aacd9a2f8f76efab2cb3b8b66866d
Rico <d-Rickyy-b@users.noreply.github.com>
2021-01-03 02:38:19 +0100

    refactor: remove unused imports

--- pyBrematic/__init__.py
+++ pyBrematic/__init__.py
@@ -1,6 +1,4 @@
-from .devices.device import Device
-from .gateways.brennenstuhl_gateway import BrennenstuhlGateway
 from .gateways.gateway import Gateway
 from .action import Action
 
-__all__ = ["Device", "Gateway", "BrennenstuhlGateway", "Action"]
+__all__ = ["Gateway", "Action"]
